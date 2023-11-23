42fb88f4764bc5392786e7294bd832ea8eb06b3f
Mauro Cubilla <mauroscubilla@gmail.com>
2023-11-22 23:45:49 -0300

    cuarta practica

---
+++ src/routes/sessions.routes.js
@@ -0,0 +1,26 @@
+import { Router } from "express";
+import passport from "passport";
+//importar el controlador de Sessions
+import { SessionsController } from "../controllers/sessions.controller.js";
+
+const router  = Router();
+
+router.post("/signup", passport.authenticate("signupStrategy", {
+    failureRedirect:"/api/sessions/fail-signup"
+}), SessionsController.successSignup);
+
+router.get("/fail-signup", SessionsController.failSignup);
+
+router.post("/login", passport.authenticate("loginStrategy", {
+    failureRedirect:"/api/sessions/fail-login"
+}), SessionsController.successLogin);
+
+router.get("/fail-login", SessionsController.failLogin);
+
+router.post("/forgot-password", SessionsController.forgotPassword);
+
+router.post("/reset-password", SessionsController.resetPassword);
+
+router.get("/logout", SessionsController.logout);
+
+export {router as sessionsRouter};
